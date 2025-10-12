660d38dd053f172bb02832be505b52300b8fd6c7
Aromal M <argonebee@gmail.com>
2025-10-12 22:14:37 +0530

    Update Supabase edge URL in config.js

--- config.js
+++ config.js
@@ -5,5 +5,5 @@
 // window.SUPABASE_EDGE_URL = "https://<project-ref>.functions.supabase.co";
 // window.SUPABASE_ANON_KEY = "<your-public-anon-key>";
 
-window.SUPABASE_EDGE_URL = "https://zdlovntmncotfxxxrtqe.supabase.co";
+window.SUPABASE_EDGE_URL = "https://zdlovntmncotfxxxrtqe.functions.supabase.co";
 window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpkbG92bnRtbmNvdGZ4eHhydHFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAxNTUyMTcsImV4cCI6MjA3NTczMTIxN30.6zge29bF-33xak18b8S8BamWDn8VSz-22KGIWad0IcU";
